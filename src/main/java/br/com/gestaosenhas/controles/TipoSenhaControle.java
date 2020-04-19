aeae27731719e72a775849b33a597727e190a015
Luiz Alves <luizhmalves@hotmail.com>
2020-04-19 11:35:05 -0300

    Inserção de serviço e controle para tipos de senhas
    Inserção de serviço e controle para tipos de senhas

---
+++ src/main/java/br/com/gestaosenhas/controles/TipoSenhaControle.java
@@ -0,0 +1,32 @@
+package br.com.gestaosenhas.controles;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.gestaosenhas.entidades.TiposSenhas;
+import br.com.gestaosenhas.servicos.TipoSenhaService;
+
+@RestController
+@RequestMapping({"/senhas"})
+public class TipoSenhaControle {
+	
+	@Autowired
+	private TipoSenhaService tpSenhaService;
+	
+	@PostMapping()
+	public TiposSenhas insereSenha(@RequestBody TiposSenhas tpSenha) {
+		return tpSenhaService.insereTpSenha(tpSenha);
+	}
+	
+	@GetMapping()
+	public List<TiposSenhas> buscaTodas() {
+		return tpSenhaService.buscaTodas();
+	}
+	
+}
